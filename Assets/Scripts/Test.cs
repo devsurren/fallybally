43d15536ab638db8018c4db7a68caf1262eb721d
devSurren <computingwithsurren@gmail.com>
2021-01-26 13:09:17 +0530

    Creating Test Script File

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Test();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Test(){
+        print("Just Use This Script File for Your Testing")
+    }
+}
