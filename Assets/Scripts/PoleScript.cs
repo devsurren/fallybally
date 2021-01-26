c8378be91dff0f96dc45e8ba3e19e92583b7c242
devSurren <computingwithsurren@gmail.com>
2021-01-26 21:32:50 +0530

    Adding Pole With Objects With Pole Rotation

---
+++ Assets/Scripts/PoleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleScript : MonoBehaviour
+{
+    [SerializeField]
+    float rotationSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Rotate(new Vector3(0,transform.position.y*rotationSpeed,0));
+    }
+}
